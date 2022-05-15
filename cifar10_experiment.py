7d7f5e8e7cfbf5cb6be19d35b37c30b3fb7c5bf2
jarroddominguez <jarrod.dominguez@gmail.com>
2022-05-15 17:49:14 -0500

    things

--- cifar10_experiment.py
+++ cifar10_experiment.py
@@ -51,13 +51,21 @@ class CIFAR10AugmentationExperiment:
         """Downloads data and applies transforms to TRAIN DATA ONLY. Also initializes `self.model`"""
         # fmt: off
         self.train_set = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=self.transforms)
-        self.test_set = torchvision.datasets.CIFAR10(root="./data", train=False, download=True)
+        self.test_set = torchvision.datasets.CIFAR10(root="./data", train=False, download=True, transform=torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    ))
         # fmt: on
 
         # Initialize an untrained ResNet18 model in `self.model`
         self.model = torch.hub.load(
             "pytorch/vision:v0.10.0", "resnet18", pretrained=False
         )
+
+        if torch.cuda.is_available():
+            self.model.cuda()
 
     def train(self, nepoch: int = 10, batch_size: int = 4, *args, **kwargs):
         """Train the ResNet18 model in `self.model`, using `self.train_set`
@@ -74,13 +82,15 @@ class CIFAR10AugmentationExperiment:
             *args,
             **kwargs,
         )
+        self.train_hist = []
+        self.val_hist = []
 
-        device = torch.device("cuda")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # Training parameters
         lr = 1e-3
-        mbsize = 256  # 16
-        max_nepochs = 250
+        mbsize = batch_size  # 16
+        max_nepochs = nepoch
         loss_fn = nn.CrossEntropyLoss()
         lookback = 10
         verbose = True
@@ -140,6 +150,9 @@ class CIFAR10AugmentationExperiment:
 
         # Train
         for epoch in range(max_nepochs):
+            n = 0
+            mean_loss = 0
+            mean_acc = 0
             for x, y in tqdm(train_loader, desc="Training loop", leave=True):
                 # Move to device.
                 x = x.to(device=device)
@@ -151,7 +164,23 @@ class CIFAR10AugmentationExperiment:
                 optimizer.step()
                 self.model.zero_grad()
 
+                n += len(x)
+                x = x.to(device)
+                y = y.to(device)
+
+                # Get predictions.
+                pred = self.model(x)
+
+                # Update loss.
+                loss = loss_fn(pred, y).item()
+                mean_loss += len(x) * (loss - mean_loss) / n
+
+                # Update accuracy.
+                acc = (torch.argmax(pred, dim=1) == y).float().mean().item()
+                mean_acc += len(x) * (acc - mean_acc) / n
+
             # Check progress.
+            self.train_hist.append(mean_acc)
             with torch.no_grad():
                 # Calculate validation loss.
                 self.model.eval()
@@ -163,6 +192,7 @@ class CIFAR10AugmentationExperiment:
                     print("Val acc = {:.4f}".format(val_acc))
                 loss_list.append(val_loss)
                 acc_list.append(val_acc)
+                self.val_hist.append(val_acc)
                 scheduler.step(val_acc)
 
                 # Check convergence criterion.
