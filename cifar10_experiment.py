cfd13bf2ac33d8f818ef1d0cd9015be3e925cb3e
jarroddominguez <jarrod.dominguez@gmail.com>
2022-05-15 17:13:24 -0500

    changes

--- cifar10_experiment.py
+++ cifar10_experiment.py
@@ -3,9 +3,26 @@ import torchvision
 from dataclasses import dataclass, field
 import tqdm
 from torch.utils.data.dataloader import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import os.path
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from copy import deepcopy
 
-
-CIFAR10_CLASSES = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
+CIFAR10_CLASSES = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
 
 
 @dataclass
@@ -38,7 +55,9 @@ class CIFAR10AugmentationExperiment:
         # fmt: on
 
         # Initialize an untrained ResNet18 model in `self.model`
-        self.model = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", pretrained=False)
+        self.model = torch.hub.load(
+            "pytorch/vision:v0.10.0", "resnet18", pretrained=False
+        )
 
     def train(self, nepoch: int = 10, batch_size: int = 4, *args, **kwargs):
         """Train the ResNet18 model in `self.model`, using `self.train_set`
@@ -56,30 +75,117 @@ class CIFAR10AugmentationExperiment:
             **kwargs,
         )
 
-        criterion = torch.nn.CrossEntropyLoss()
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+        device = torch.device("cuda")
 
-        # Train for `nepoch` loops... tqdm progress bar added to track
-        for epoch in tqdm.trange(nepoch):
-            running_loss = 0.0
-            for i, data in enumerate(train_loader):
-                # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data
+        # Training parameters
+        lr = 1e-3
+        mbsize = 256  # 16
+        max_nepochs = 250
+        loss_fn = nn.CrossEntropyLoss()
+        lookback = 10
+        verbose = True
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+        # Validation function
+        val_loader = DataLoader(
+            self.test_set, batch_size=mbsize, shuffle=False, num_workers=4
+        )
 
-                # forward + backward + optimize
-                outputs = self.model(inputs)
-                loss = criterion(outputs, labels)
+        def validate(model):
+            n = 0
+            mean_loss = 0
+            mean_acc = 0
+
+            for x, y in val_loader:
+                # Move to GPU.
+                n += len(x)
+                x = x.to(device)
+                y = y.to(device)
+
+                # Get predictions.
+                pred = model(x)
+
+                # Update loss.
+                loss = loss_fn(pred, y).item()
+                mean_loss += len(x) * (loss - mean_loss) / n
+
+                # Update accuracy.
+                acc = (torch.argmax(pred, dim=1) == y).float().mean().item()
+                mean_acc += len(x) * (acc - mean_acc) / n
+
+            return mean_loss, mean_acc
+
+        # Data loader
+        train_loader = DataLoader(
+            self.train_set,
+            batch_size=mbsize,
+            shuffle=True,
+            drop_last=True,
+            num_workers=4,
+        )
+
+        # Setup
+        optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            factor=0.5,
+            patience=lookback // 2,
+            min_lr=1e-5,
+            mode="max",
+            verbose=verbose,
+        )
+        loss_list = []
+        acc_list = []
+        min_criterion = np.inf
+        min_epoch = 0
+
+        # Train
+        for epoch in range(max_nepochs):
+            for x, y in tqdm(train_loader, desc="Training loop", leave=True):
+                # Move to device.
+                x = x.to(device=device)
+                y = y.to(device=device)
+
+                # Take gradient step.
+                loss = loss_fn(self.model(x), y)
                 loss.backward()
                 optimizer.step()
+                self.model.zero_grad()
 
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:  # print every 2000 mini-batches
-                    print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
-                    running_loss = 0.0
+            # Check progress.
+            with torch.no_grad():
+                # Calculate validation loss.
+                self.model.eval()
+                val_loss, val_acc = validate(self.model)
+                self.model.train()
+                if verbose:
+                    print("----- Epoch = {} -----".format(epoch + 1))
+                    print("Val loss = {:.4f}".format(val_loss))
+                    print("Val acc = {:.4f}".format(val_acc))
+                loss_list.append(val_loss)
+                acc_list.append(val_acc)
+                scheduler.step(val_acc)
+
+                # Check convergence criterion.
+                val_criterion = -val_acc
+                if val_criterion < min_criterion:
+                    min_criterion = val_criterion
+                    min_epoch = epoch
+                    best_model = deepcopy(self.model)
+                    print("")
+                    print("New best epoch, acc = {:.4f}".format(val_acc))
+                    print("")
+                elif (epoch - min_epoch) == lookback:
+                    if verbose:
+                        print("Stopping early")
+                    break
+
+        # Keep best model
+        self.model = best_model
+
+        # Save model
+        self.model.cpu()
+        torch.save(self.model, "cifar resnet.pt")
+        self.model.to(device)
 
     def evaluate_model(self) -> None:
         """Evaluate the model. Predict model over test set and produce diagnostic plots, tables, etc."""
